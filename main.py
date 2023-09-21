2271ee70254a3bacf4bf60c02d154aed0a2922ed
pl23 <61396518+pl23@users.noreply.github.com>
2023-09-21 17:29:23 -0400

    added add_to_boerd

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from typing import List
-
 import pygame
 import getpass as gp
 import socket
+from pprint import pprint
+
 import numpy
 
 SystemUser = gp.getuser()
-Peace_index = 0
 
-pygame.init()
-pygame.display.init()
-window_surface = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
-clock = pygame.time.Clock()
-pygame.display.set_caption("game")
+# pygame.init()
+# pygame.display.init()
+# window_surface = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+# clock = pygame.time.Clock()
+# pygame.display.set_caption("game")
 GRID_WIDTH, GRID_HEIGHT = 10, 20
-x, y = 0, 0
 
-grid = [
+display_board = [
+    [0 for _ in range(GRID_WIDTH)]
+    for _ in range(GRID_HEIGHT)
+]
+board = [
     [0 for _ in range(GRID_WIDTH)]
     for _ in range(GRID_HEIGHT)
 ]
@@ -206,6 +208,34 @@ Z_piece = [
 piece_list = [I_piece, J_piece, L_piece, O_piece, S_piece, T_piece, Z_piece]
 
 
+def add_to_bord(x,y):
+    count_x = 0
+    count_y = 0
+    piece_tipe = piece_list[0]
+    Piece_Rotate = piece_tipe[0]
+
+
+    while count_x != 4:
+        board_row = board[count_x]
+        Piece_Row = Piece_Rotate[count_x]
+
+        while count_y != 4:
+            board_cell = board_row[count_y]
+            Piece_cell = Piece_Row[count_y]
+
+            if Piece_cell == 1:
+                board[x][y] += 1
+            count_y += 1
+        count_x +=1
+        count_y = 0
+
+
+
+
+    pprint(board)
+
+
+
 def main():
     running = True
     while running:
@@ -217,7 +247,7 @@ def main():
                 window_surface = pygame.display.set_mode((new_width, new_height), pygame.RESIZABLE)
 
         update()
-        clock.tick(60)
+        # clock.tick(60)
 
 
 def update():
@@ -225,8 +255,8 @@ def update():
     print(pygame.key.get_focused())
 
 
-
 if __name__ == "__main__":
     # main()
     # pygame.QUIT()
+    add_to_bord(0,0)
     pass
